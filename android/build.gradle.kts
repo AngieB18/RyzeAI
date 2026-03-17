97764bb1bebee8467a4598901625dc2dc646dc79
KevDev-exe <kevinyamid2005@gmail.com>
2026-03-16 21:44:19 -0500

    feat: login page and assets added

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,7 +12,7 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 plugins {
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 subprojects {
